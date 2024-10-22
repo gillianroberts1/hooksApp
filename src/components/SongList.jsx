d1ec96fc86ab05d3335381748000ee5ac3c1c735
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2024-10-22 22:03:23 +0100

    useEffect

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid"; // Updated import for uuidv4
 import NewSongForm from "./NewSongForm";
 
@@ -9,9 +9,18 @@ const SongList = () => {
     { title: "this wild darkness", id: 3 },
   ]);
 
+  const [age, setAge] = useState(20);
+
   const addSong = (title) => {
     setSongs([...songs, { title, id: uuidv4() }]);
   };
+  useEffect(() => {
+    console.log("useEffecthook ran", songs);
+  }, [songs]);
+  useEffect(() => {
+    console.log("useEffecthook ran", age);
+  }, [age]);
+
   return (
     <div className="song-list">
       <ul>
@@ -19,7 +28,8 @@ const SongList = () => {
           return <li key={song.id}>{song.title}</li>;
         })}
       </ul>
-      <NewSongForm addSong={addSong}/>
+      <NewSongForm addSong={addSong} />
+      <button onClick={() => setAge(age + 1)}>Add 1 to age: {age}</button>
     </div>
   );
 };
